e1c92decdcac58131c7b7c0c1ee0644bc43e9c38
Marc Emmanuel <marc.emmanuel@virtual-identity.com>
2020-01-02 18:12:24 +0100

    feat: 🎸 Add choice for older version

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const { runner } = require('hygen');
-const Logger = require('hygen/lib/logger');
-const path = require('path')
-const defaultTemplates = path.join(__dirname, './_templates');
+const inquirer = require("inquirer");
+const { runner } = require("hygen");
+const Logger = require("hygen/lib/logger");
+const path = require("path");
+const defaultTemplates = path.join(__dirname, "./_templates");
 
-module.exports = () => runner('component with-prompt', {
+module.exports = async () => {
+  const answers = await inquirer.prompt([
+    {
+      type: "list",
+      name: "version",
+      message: "Which biotope-element version do you want to generate for?",
+      default: ["4"],
+      choices: ["4", "<=3"]
+    }
+  ]);
+
+  let generator = "component_v4";
+  if (answers.version === "<=3") {
+    generator = "component";
+  }
+  
+  runner(`${generator} with-prompt`, {
     templates: defaultTemplates,
     cwd: process.cwd(),
     logger: new Logger(console.log.bind(console)),
     debug: !!process.env.DEBUG,
-    createPrompter: () => require('inquirer'),
-});
+    createPrompter: () => require("inquirer")
+  });
+};
