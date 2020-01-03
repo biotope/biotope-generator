e2b2f6713327a486b81130915e2350ea8956806b
Marc Emmanuel <marc.emmanuel@virtual-identity.com>
2020-01-03 07:58:48 +0100

    feat: 🎸 Add biotope-quality-gate

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const { runner } = require('hygen');
 const Logger = require('hygen/lib/logger');
 const path = require('path')
 const defaultTemplates = path.join(__dirname, './_templates');
+const inquirer = require('inquirer');
 
 module.exports = () => runner('component with-prompt', {
     templates: defaultTemplates,
     cwd: process.cwd(),
+    // eslint-disable-next-line no-console
     logger: new Logger(console.log.bind(console)),
     debug: !!process.env.DEBUG,
-    createPrompter: () => require('inquirer'),
+    createPrompter: () => inquirer,
 });
